5e8311fb5184514b12dbf1eb5b2e4a12e4acb113
Unknown <riza.ega.s@mail.ugm.ac.id>
2017-07-09 16:44:29 +0700

    Add Reducers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function(){
+    return [
+        {title: "JavaScript the weird parts"},
+        {title: "Harry Potter Frameworks "},
+        {title: "The Dark Tower"},
+        {title: "Bilbo and Friends"},
+        {title: "Elliot the Daring Mr Robot"}
+    ]
+}
